095a4657418224b4fd1b1a9c42527bc36ce70004
magomed <mr.fila@gmail.com>
2020-11-01 01:14:08 +0300

    Валидация файлов, прием от фронта файлов в base64 pdf, jpg, jpeg, png, tiff

--- project/ocr/serializers.py
+++ project/ocr/serializers.py
@@ -1,7 +1,10 @@
+import logging
+
 from rest_framework import serializers
 from drf_extra_fields.fields import Base64FileField
-from ocr.models import Document, DocumentType, PageDocument
 
+from ocr.models import Document, DocumentType, PageDocument
+from .utils import is_pdf
 
 class DocumentTypeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -27,10 +30,37 @@ class PageDocumentSerializer(serializers.ModelSerializer):
         read_only_fields = ["text", "doc_text", "data"]
 
 
+class DocumentBase64FileField(Base64FileField):
+    ALLOWED_TYPES = ['pdf', 'png', 'jpg', 'jpeg', 'tiff']
+
+    def get_file_extension(self, filename, decoded_file):
+        import imghdr
+        from PyPDF2 import PdfFileReader
+        from PyPDF2.utils import PdfReadError
+        import io
+
+        try:
+            PdfFileReader(io.BytesIO(decoded_file))
+            return 'pdf'
+        except PdfReadError as e:
+            logging.warning(e)
+
+        try:
+            extension = imghdr.what(filename, decoded_file)
+            return 'jpg' if extension == 'jpeg' else extension
+        except FileExistsError as e:
+            logging.warning(e)
+        try:
+            extension = imghdr.what(filename, decoded_file)
+            return 'tiff' if extension == 'tiff' else extension
+        except FileExistsError as e:
+            logging.warning(e)
+
+
 class DocumentSerializer(serializers.ModelSerializer):
     status = serializers.ReadOnlyField(source='get_status_display')
     pages = PageDocumentSerializer(many=True, read_only=True)
-    file = Base64FileField(required=True)
+    file = DocumentBase64FileField(required=True)
 
     class Meta:
         model = Document
