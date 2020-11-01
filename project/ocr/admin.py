aae662cd636cfb9d93d1cba936d6c6cf4351ca9d
magomed <mr.fila@gmail.com>
2020-11-01 11:44:46 +0300

    Поисковая строка в админке

--- project/ocr/admin.py
+++ project/ocr/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import (Document, DocumentType, PageDocument)
-
+import re
 
 @admin.register(DocumentType)
 class DocumentTypeAdmin(admin.ModelAdmin):
@@ -16,5 +16,21 @@ class DocumentAdmin(admin.ModelAdmin):
 @admin.register(PageDocument)
 class PageDocumentAdmin(admin.ModelAdmin):
     search_fields = ('text', )
-    list_display = ('id', 'parent_document', 'page', 'file', 'type', 'status')
+    list_display = ('id', 'parent_document', 'page', 'file', 'type', 'status', 'search_result')
     list_filter = ('type', 'status', 'page')
+
+    def changelist_view(self, request, extra_context=None):
+        self.q = request.GET.get('q')
+        return super(PageDocumentAdmin, self).changelist_view(request, extra_context=extra_context)
+
+    def search_result(self, obj):
+        """Метод который добавялем столбец в котором выводиться кусок текста
+        с вхождением строки поиска + n кол-во символов"""
+        n = 30 # Количество символов
+        result = ""
+        if self.q:
+            p = re.compile(self.q, re.IGNORECASE)
+            for m in p.finditer(obj.text):
+                start, end = m.span()
+                result += "[{}], ".format(obj.text[start:end + n])
+        return result
