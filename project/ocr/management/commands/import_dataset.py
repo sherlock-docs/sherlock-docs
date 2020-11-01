94f7135f09a85f1d9ec5e9854a86095e1eb7f149
magomed <mr.fila@gmail.com>
2020-11-01 05:05:53 +0300

    Импорт Датасета для распознания через tesseract

---
+++ project/ocr/management/commands/import_dataset.py
@@ -0,0 +1,22 @@
+import os
+
+from django.core.management import BaseCommand
+from django.core.files import File
+
+from ocr.models import Document, PageDocument
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        Document.objects.all().delete()
+        PageDocument.objects.all().delete()
+        dataset_path = os.path.join("/user", "src", "app", "Dataset")
+        for file_type in os.listdir(dataset_path):
+            path_to_file = os.path.join(dataset_path, file_type)
+            for file in os.listdir(path_to_file):
+                filepath = os.path.join(path_to_file, file)
+                page, created = Document.objects.get_or_create(file=filepath)
+                # Прикладываем файл.
+                with open(self.file.path, 'rb') as f:
+                    filename = os.path.splitext(os.path.basename(self.file.path))[0]
+                    page.file.save(file, File(f), save=True)
