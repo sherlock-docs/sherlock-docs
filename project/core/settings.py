bb29b8c241814a60c2d51c2d9841d280563f7d39
magomed <mr.fila@gmail.com>
2020-11-01 07:38:50 +0300

    Правки ошибок в API PageDocument

--- project/core/settings.py
+++ project/core/settings.py
@@ -49,12 +49,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.IsAuthenticated',
+    # ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ],
 }
 
 MIDDLEWARE = [
--- project/ocr/filters.py
+++ project/ocr/filters.py
@@ -11,10 +11,10 @@ class DocumentFilter(django_filters.FilterSet):
 
 
 class PageDocumentFilter(django_filters.FilterSet):
-    type_id = django_filters.Filter(name='type_id', lookup_expr='exact')
-    type_name = django_filters.Filter(name='type__name', lookup_expr='exact')
-    text = django_filters.Filter(name='text', lookup_expr='icontains')
-    doc_page = django_filters.Filter(name='page', lookup_expr='exact')
+    type_id = django_filters.Filter(field_name='type_id', lookup_expr='exact')
+    type_name = django_filters.Filter(field_name='type__name', lookup_expr='exact')
+    text = django_filters.Filter(field_name='text', lookup_expr='icontains')
+    doc_page = django_filters.Filter(field_name='page', lookup_expr='exact')
 
     class Meta:
         model = PageDocument
