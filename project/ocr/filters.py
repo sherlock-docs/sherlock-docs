93ee4252d859593c73e466f910f23e68f75293fd
magomed <mr.fila@gmail.com>
2020-11-01 07:13:02 +0300

    Фильтр по номеру страницы.

--- project/ocr/filters.py
+++ project/ocr/filters.py
@@ -14,6 +14,7 @@ class PageDocumentFilter(django_filters.FilterSet):
     type_id = django_filters.Filter(name='type_id', lookup_expr='exact')
     type_name = django_filters.Filter(name='type__name', lookup_expr='exact')
     text = django_filters.Filter(name='text', lookup_expr='icontains')
+    doc_page = django_filters.Filter(name='page', lookup_expr='exact')
 
     class Meta:
         model = PageDocument
