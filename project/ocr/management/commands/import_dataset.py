4e38e8fcb62a1152fc58d3a33e973ee5c51ef7a1
magomed <mr.fila@gmail.com>
2020-11-01 06:11:29 +0300

    Поменял порт редиса, поправил скрипт импорта датасета.

--- project/ocr/management/commands/import_dataset.py
+++ project/ocr/management/commands/import_dataset.py
@@ -1,21 +1,17 @@
 import os
-
 from django.core.management import BaseCommand
-from django.core.files import File
-
 from ocr.models import Document, PageDocument
-
+from django.core.files import File
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         Document.objects.all().delete()
         PageDocument.objects.all().delete()
-        dataset_path = os.path.join("/user", "src", "app", "Dataset")
+        dataset_path = os.path.join("/usr", "src", "app", "media", "docs", "Dataset")
         for file_type in os.listdir(dataset_path):
             path_to_file = os.path.join(dataset_path, file_type)
             for file in os.listdir(path_to_file):
                 filepath = os.path.join(path_to_file, file)
-                page, created = Document.objects.get_or_create(file=filepath)
-                # Прикладываем файл.
+                doc, created = Document.objects.get_or_create(file=filepath)
                 with open(filepath, 'rb') as f:
-                    page.file.save(file, File(f), save=True)
+                    doc.file.save(file, File(f), save=True)
